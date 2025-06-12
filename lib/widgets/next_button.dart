af20f3200aea7f6464cd143023852d58c5a3d311
Jordan Bonnet <jordan.bonnet@gmail.com>
2025-06-12 11:37:09 +0200

    put loading indicator in the center in next button

--- lib/widgets/next_button.dart
+++ lib/widgets/next_button.dart
@@ -18,32 +18,19 @@ class NextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = context.read<FluoTheme>();
-
-    Widget? loadingIndicator;
-    if (loading) {
-      loadingIndicator = SizedBox(
-        width: theme.nextButtonProgressIndicatorSize.width,
-        height: theme.nextButtonProgressIndicatorSize.height,
-        child: CircularProgressIndicator(
-          color: theme.nextButtonProgressIndicatorColor,
-          strokeWidth: theme.nextButtonProgressIndicatorStrokeWidth,
-        ),
-      );
-    }
-
     return FilledButton(
       style: theme.nextButtonStyle,
       onPressed: enabled ? _onPressedIfNotLoading : null,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Text(FluoLocalizations.of(context)!.next),
-          ),
-          if (loadingIndicator != null) loadingIndicator,
-        ],
-      ),
+      child: loading
+          ? SizedBox(
+              width: theme.nextButtonProgressIndicatorSize.width,
+              height: theme.nextButtonProgressIndicatorSize.height,
+              child: CircularProgressIndicator(
+                color: theme.nextButtonProgressIndicatorColor,
+                strokeWidth: theme.nextButtonProgressIndicatorStrokeWidth,
+              ),
+            )
+          : Text(FluoLocalizations.of(context)!.next),
     );
   }
 
